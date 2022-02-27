74f2a45ee5859d82715ccedc93962e03867ecf38
guillaume.macke@gmail.com <github2601Leo*>
2022-02-27 21:36:21 +0100

    Ajout initial

---
+++ choco-install.bat
@@ -0,0 +1,45 @@
+echo "Firefox"
+choco install firefox -y
+
+echo "google chrome"
+choco install googlechrome -y
+
+echo "jetbrainstoolbox"
+choco install jetbrainstoolbox -y
+
+echo "VLC"
+choco install vlc -y
+
+echo "7Zip"
+choco install 7zip -y
+
+echo "Steam"
+choco install steam -y
+
+echo "EpicGames"
+choco install epicgameslauncher -y
+
+
+echo "Notepad++"
+choco install "notepadplusplus -y
+
+echo "keepass"
+choco install keepass -y
+
+echo "Git"
+choco install git -y
+
+echo "Maven"
+choco install maven -y
+
+echo "Java 8"
+choco install jdk8 -y
+
+echo "Openjdk last version"
+choco install openjdk -y
+
+echo "Figma"
+choco install figma -y
+
+echo "evernote"
+choco install evernote -y
